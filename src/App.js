f8323f50e615de5fb1625fac954e16a5761b88fe
Shivani Bandi <141583625+shivanibandi110@users.noreply.github.com>
2024-02-11 21:43:26 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,77 @@
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [city, setCity] = useState('');
+  const [weather, setWeather] = useState(null);
+  const [error, setError] = useState('');
+
+  const API_KEY = '21c6e7f2e94757b67bb22ddf5d71cbab';
+
+  const getWeather = async () => {
+    try {
+      const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
+      const data = await response.json();
+      if (data.cod === 200) {
+        setWeather(data);
+        setError('');
+      } else {
+        setError(data.message);
+      }
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      setError('Error fetching data. Please try again later.');
+    }
+  };
+
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <h3> Enter required location </h3>
+      <h4>This is simple weatherapp .The location is verified and weather is given by using openweathermap api</h4>
+      <input
+        type="text"
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
+        placeholder="Enter city name"
+      />
+      <button onClick={getWeather}>Get Weather</button>
+      {/* <img src='weather.png' alt='weather images'></img> */}
+      {weather && (
+        <div>
+          <h2>{weather.name}, {weather.sys.country}</h2>
+          <p>{weather.weather[0].description}</p>
+          <p>Temperature: {weather.main.temp}°C</p>
+        </div>
+      )}
+      {error && <p>{error}</p>}
+    </div>
+  );
+}
+
+export default App;
